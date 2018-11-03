21dce752a0c9f4362f29018712a8209d85db3efa
evgeniy.ivchenko <ivchenkoevgeniy@gmail.com>
2018-11-03 16:54:32 +0300

    Singleton example has added

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s = Singleton.Instance;
+
+            s.Count = 42;
+
+            Console.WriteLine(s.Count.ToString());
+
+            test();
+        }
+
+        static void test()
+        {
+            Singleton s = Singleton.Instance;
+            Console.WriteLine(s.Count.ToString());
+        }
+
+    }
+
+    class Singleton
+    {
+        public int Count { get; set; }
+
+        private static Singleton _singleton;
+
+        static Singleton()
+        {
+            Console.WriteLine("static Singleton");
+            _singleton = new Singleton();
+        }
+
+        public static Singleton Instance
+        {
+            get { return _singleton; }
+        }
+
+        private Singleton()
+        {
+        }
+    }
+}
